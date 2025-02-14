f8c03cfef444795c7176241724d5f95273103fe8
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-14 16:07:53 +0900

    chore: eslint, prettier 설정
    - `eslint-plugin-simple-import-sort`을 추가하여 import/export 정렬 적용
    - `eslint-plugin-unused-imports`을 추가하여 미사용 import 자동 제거
    - ESLint 설정을 업데이트하여 해당 플러그인 활성화
    - `prettier` 및 `prettier-plugin-tailwindcss` 설치 및 설정 적용
    - `.prettierignore`, `.prettierrc` 파일 추가하여 코드 스타일 일관성 유지

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -16,6 +18,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -23,6 +27,14 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'simple-import-sort/imports': 'error', // import 정렬
+      'simple-import-sort/exports': 'error', // export 정렬
+      'no-unused-vars': 'off', // 기본 규칙 비활성화
+      '@typescript-eslint/no-unused-vars': 'off', // TypeScript의 미사용 변수 규칙 비활성화
+      'unused-imports/no-unused-imports': 'error', // 미사용 import 제거
+      'unused-imports/no-unused-vars': 'error',
+      'react/react-in-jsx-scope': 'off', // React 17+ JSX 트랜스폼 설정
+      '@typescript-eslint/no-empty-object-type': 'off',
     },
-  },
-)
+  }
+);
