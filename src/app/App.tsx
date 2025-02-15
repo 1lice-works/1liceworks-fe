9737af16dafa67c6f585b887aa99b7f8361ba554
G-jjunny <rudwns9551@naver.com>
2025-02-15 18:34:19 +0900

    feat: app.tsx import오류 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { QueryProvider } from './providers/QueryProvider';
-import { router } from './routes/Router';
+import { router } from './routes/router';
 
 export const App = () => (
   <QueryProvider>
