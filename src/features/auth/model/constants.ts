3b8a836c2e5a5a3bba49f40a9c6ce210a4ce1df7
G-jjunny <rudwns9551@naver.com>
2025-02-17 20:15:46 +0900

    feat: form constants 주석 추가

--- src/features/auth/model/constants.ts
+++ src/features/auth/model/constants.ts
@@ -1,12 +1,12 @@
 export const AUTH_FORM_STYLES = {
-  form: 'flex flex-col items-center gap-10 w-[338px]',
-  title: 'text-foreground text-xl font-bold text-center',
-  inputLayer: 'flex flex-col gap-3 w-full',
-  label: ' text-sm font-semibold mb-1',
-  inputAndButton: 'mb-1 flex gap-1',
+  form: 'flex flex-col items-center gap-10 w-[338px]', // form 전체 레이아웃
+  title: 'text-foreground text-xl font-bold text-center', // form의 title
+  inputLayer: 'flex flex-col gap-3 w-full', // 2개 이상의 input
+  label: ' text-sm font-semibold mb-1', // input의 label
+  inputAndButton: 'mb-1 flex gap-1', // input과 button이 동시에 있을때
 
-  submit: 'w-full flex flex-col gap-2',
-  already: 'text-muted-foreground flex justify-between',
+  submit: 'w-full flex flex-col gap-2', // submit 영역
+  already: 'text-muted-foreground flex justify-between', // submit 하위 text스타일
 
   checkBox: 'flex items-center gap-2',
 
