450e00a78ac5703a93fa55790fda62164a9ee1c6
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-17 18:23:43 +0900

    design: MainLayout 구조 개선
    - LNB 및 main 콘텐츠 영역(Outlet)에 flex-1 적용하여 스크롤 방지
    - GNB, LNB, Outlet 영역에 패딩 추가
    - Outlet 영역이 GNB와 LNB를 제외한 전체 화면을 차지하도록 조정

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -2,14 +2,15 @@ import { Outlet } from 'react-router-dom';
 
 export const MainLayout = () => {
   return (
-    <div className='h-screen w-full'>
+    <div className='flex h-screen w-full flex-col'>
       {/* GNB 위치 (아래 코드는 수정 예정) */}
-      <div className='bg-background-dark w-full text-white'>GNB</div>
-      <div className='flex h-full w-full'>
-        {/* LNB 위치 (아래 코드는 수정 예정) */}
-        <div className='bg-background-light'>LNB</div>
+      <div className='bg-background-dark w-full px-10 py-4 text-white'>GNB</div>
 
-        <main className='flex-1'>
+      <div className='flex flex-1'>
+        {/* LNB 위치 (아래 코드는 수정 예정) */}
+        <div className='bg-background-light h-full p-4'>LNB</div>
+
+        <main className='h-full flex-1 p-4'>
           <Outlet />
         </main>
       </div>
