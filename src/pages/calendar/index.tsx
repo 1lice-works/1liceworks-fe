ef2d74fb50f93bdfcc8e0bb4732e0a66a85313a0
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-18 21:56:38 +0900

    feat: 캘린더 페이지 기본 UI 추가

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,3 +1,14 @@
-export function CalendarPage() {
-  return <div>calendar Page</div>;
-}
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+
+const localizer = momentLocalizer(moment);
+
+export const CalendarPage = () => {
+  return (
+    <div className='h-full'>
+      <Calendar localizer={localizer} />
+    </div>
+  );
+};
