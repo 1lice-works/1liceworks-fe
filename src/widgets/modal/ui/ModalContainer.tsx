e005f1d2e909d64540a884b9e0578bc52c42ab1a
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-20 22:35:35 +0900

    feat: 모달 UI 구현

---
+++ src/widgets/modal/ui/ModalContainer.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/shared/ui/shadcn/Button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/shared/ui/shadcn/Card';
+import { Input } from '@/shared/ui/shadcn/Input';
+import { Separator } from '@/shared/ui/shadcn/Separator';
+
+export const ModalContainer = () => {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-black/30'>
+      <Card className='w-1/3 min-w-fit'>
+        <CardHeader>
+          <CardTitle className='text-xl'>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Separator />
+          <div className='flex flex-col gap-y-4 pt-6'>
+            <Input />
+            <Input />
+            <Input />
+          </div>
+        </CardContent>
+        <CardFooter className='flex justify-end space-x-3'>
+          <Button variant='outline'>취소</Button>
+          <Button>확인</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
