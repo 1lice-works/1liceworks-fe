8d7fa62894d0ee842bce5fe81083c83791be7ceb
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-21 01:43:53 +0900

    feat: 오버레이 클릭 시에만 모달이 닫히도록 이벤트 버블링 제어
    - ModalContainer 클릭 시 모달이 닫히지 않도록 수정
    - 터치 이벤트 처리를 위해 onTouchStart 추가

--- src/widgets/modal/ui/ModalManager.tsx
+++ src/widgets/modal/ui/ModalManager.tsx
@@ -1,16 +1,25 @@
+import React from 'react';
+
 import { useModalStore } from '../model/useModalStore';
 import { ModalContainer } from './ModalContainer';
 
 export const ModalManager = () => {
-  const isModalOpen = useModalStore((state) => state.isOpen);
+  const isOpen = useModalStore((state) => state.isOpen);
   const closeModal = useModalStore((state) => state.closeModal);
 
-  if (!isModalOpen) return null;
+  if (!isOpen) return null;
+
+  const handleOverlayClick = (e: React.MouseEvent | React.TouchEvent) => {
+    if (e.target === e.currentTarget) {
+      closeModal();
+    }
+  };
 
   return (
     <div
       className='fixed inset-0 flex items-center justify-center bg-black/30'
-      onClick={closeModal}
+      onClick={handleOverlayClick}
+      onTouchStart={handleOverlayClick}
     >
       <ModalContainer />
     </div>
