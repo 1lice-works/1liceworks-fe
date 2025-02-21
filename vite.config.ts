749fdea06b5905dc48407850ee159b5114185457
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-21 18:21:27 +0900

    chore: vite-plugin-svgo 설치 및 설정 추가
    - 기존에 설치했던 svgo는 삭제
    - 빌드시 svg 파일 자동 최적화되도록 설정
      - convertColors: false (색상 최적화 X)
      - removeViewBox: false (viewBox 유지)
      - cleanupIds: false (ID 최적화 X)
    - 빌드 오류로 최적화 성공 여부 확인하지 못함. 빌드 오류 해결 후 확인 예정

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,31 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
+import svgo from 'vite-plugin-svgo';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgo({
+      multipass: true,
+      plugins: [
+        {
+          name: 'preset-default',
+          params: {
+            overrides: {
+              convertColors: false,
+              removeViewBox: false,
+              cleanupIds: false,
+            },
+          },
+        },
+      ],
+    }),
+    svgr(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
