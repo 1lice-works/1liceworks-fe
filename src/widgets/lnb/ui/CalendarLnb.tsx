696978819dc06878a875fc0bcc1a4a3f1a6c2d5e
[PT1] 양혜림 <hlhlstar@naver.com>
2025-02-27 14:31:39 +0900

    feat: 캘린더 LNB 퍼블리싱
    - 각 캘린더 좌측 원형 체크박스는 추후에 추가 예정
    - 요소들 각각 컴포넌트로 분리 예정

--- src/widgets/lnb/ui/CalendarLnb.tsx
+++ src/widgets/lnb/ui/CalendarLnb.tsx
@@ -1,3 +1,80 @@
+import { ArrowUp, CalendarDays, Plus } from 'lucide-react';
+
+import { cn } from '@/shared/lib/utils';
+import { InputWithButton } from '@/shared/ui/custom/InputWithButton';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/shared/ui/shadcn/Accordion';
+import { Button } from '@/shared/ui/shadcn/Button';
+import { Calendar } from '@/shared/ui/shadcn/Calendar';
+
 export const CalendarLnb = () => {
-  return <div className='bg-background-light h-full p-4'>LNB</div>;
+  return (
+    <div className='flex h-full flex-col gap-y-4 border-r p-4'>
+      <Button
+        variant='outline'
+        size='lg'
+        className={cn(`h-0 w-fit rounded-xl py-6`)}
+      >
+        <Plus />
+        <p>만들기</p>
+      </Button>
+
+      <Calendar className={cn(`rounded-md border`)} />
+
+      <InputWithButton
+        inputProps={{
+          placeholder: '작성한 대로 일정을 생성해 드릴게요.',
+          className: 'pr-9',
+        }}
+        buttonProps={{
+          children: <ArrowUp size={16} />,
+          size: 'sm',
+          className: cn(
+            `rounded-full absolute inset-y-0 right-3 my-auto p-0 size-5`
+          ),
+        }}
+        wrapperClassName={cn(`relative space-x-0`)}
+      />
+
+      <div>
+        <Accordion type='multiple' className='w-full'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger>
+              <div className={cn(`flex items-center gap-x-2`)}>
+                <CalendarDays className='size-4' />
+                My Calendar
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>내 캘린더</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-2'>
+            <AccordionTrigger>
+              <div className={cn(`flex items-center gap-x-2`)}>
+                <CalendarDays className='size-4' />
+                Team Calendar
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>팀 캘린더</AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-3'>
+            <AccordionTrigger>
+              <div className={cn(`flex items-center gap-x-2`)}>
+                <CalendarDays className='size-4' />
+                Other Calendars
+              </div>
+            </AccordionTrigger>
+            <AccordionContent>정경준/대리</AccordionContent>
+            <AccordionContent>정태승/주임</AccordionContent>
+            <AccordionContent>명광호/사원</AccordionContent>
+            <AccordionContent>엄수경/사원</AccordionContent>
+            <AccordionContent>대한민국 법정 공휴일</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
 };
