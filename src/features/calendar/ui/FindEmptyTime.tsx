95883de23b71395c4b1b97f72e46bed7cc5c95e6
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-06 22:07:29 +0900

    refactor: 하드코딩된 calendarType을 CalendarTypeDTO를 이용하도록 수정

--- src/features/calendar/ui/FindEmptyTime.tsx
+++ src/features/calendar/ui/FindEmptyTime.tsx
@@ -1,19 +1,21 @@
+import { CalendarTypeDTO } from '@/features/calendar/api/dto';
 import { Button } from '@/shared/ui/shadcn/Button';
+
 interface FindEmptyTimeProps {
-  calendarType: number;
+  calendarType: CalendarTypeDTO;
 }
 
 export const FindEmptyTime = ({ calendarType }: FindEmptyTimeProps) => {
   return (
-    <div className='flex h-full w-full max-w-[340px] flex-col items-center justify-center gap-4 rounded-lg border-2 border-[#CBD5E1] bg-[#F9FBFD]'>
-      {calendarType === 1 && (
+    <div className='bg-background-light flex h-full w-full max-w-80 flex-col items-center justify-center gap-4 rounded-lg border'>
+      {calendarType === 'TEAM' && (
         <>
-          <p className='text-muted-foreground text-xs'>
+          <p className='text-muted-foreground text-sm'>
             회의 시간과 참석자를 선택하셨나요?
             <br />
             모두가 가능한 회의 시간을 찾아보세요.
           </p>
-          <Button>빈 시간 찾기</Button>
+          <Button size='sm'>빈 시간 찾기</Button>
         </>
       )}
     </div>
