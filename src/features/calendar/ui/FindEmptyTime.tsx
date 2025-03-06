e21d05b5d356c2adb79f222ffbad76250ac08c06
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-06 22:16:53 +0900

    fix: 상세 페이지에서 props 넘기는 방식 호환되지 않아 발생한 빌드 오류 해결
    - '하드코딩된 calendarType을 CalendarTypeDTO를 이용하도록 수정' 작업 취소
    - calendarType을 숫자형으로 넘기는 방식으로 원상복구

--- src/features/calendar/ui/FindEmptyTime.tsx
+++ src/features/calendar/ui/FindEmptyTime.tsx
@@ -1,14 +1,14 @@
-import { CalendarTypeDTO } from '@/features/calendar/api/dto';
 import { Button } from '@/shared/ui/shadcn/Button';
 
 interface FindEmptyTimeProps {
-  calendarType: CalendarTypeDTO;
+  // calendarType: CalendarTypeDTO;
+  calendarType: number;
 }
 
 export const FindEmptyTime = ({ calendarType }: FindEmptyTimeProps) => {
   return (
     <div className='bg-background-light flex h-full w-full max-w-80 flex-col items-center justify-center gap-4 rounded-lg border'>
-      {calendarType === 'TEAM' && (
+      {calendarType === 1 && (
         <>
           <p className='text-muted-foreground text-sm'>
             회의 시간과 참석자를 선택하셨나요?
