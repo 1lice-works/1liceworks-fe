3892f50f324227a6ec77aaa4524cb83bc6a12319
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-06 20:30:39 +0900

    feat: 캘린더 일정 관련 DTO 정의

--- src/features/calendar/api/dto.ts
+++ src/features/calendar/api/dto.ts
@@ -1,4 +1,6 @@
 export type CalendarTypeDTO = 'TEAM' | 'MEMBER' | 'OTHER';
+export type PrivacyTypeDTO = 'PUBLIC' | 'PRIVATE';
+export type AvailabilityDTO = 'FREE' | 'BUSY';
 
 export interface CalendarDTO {
   name: string;
@@ -9,3 +11,20 @@ export interface CalendarDTO {
 }
 
 export type CalendarListDTO = CalendarDTO[];
+
+export interface EventDTO {
+  eventId: number;
+  title: string;
+  description: string;
+  dtStartTime: string; // ISO 8601 형식의 타임스탬프 문자열 (2025-03-06T01:16:56.032Z)
+  dtEndTime: string; // ISO 8601 형식의 타임스탬프 문자열
+  isAllDay: boolean;
+  privacyType: PrivacyTypeDTO;
+  availability: AvailabilityDTO;
+  location: string;
+}
+
+export interface CalendarEventsDTO {
+  calendarId: number;
+  eventDtos: EventDTO[];
+}
