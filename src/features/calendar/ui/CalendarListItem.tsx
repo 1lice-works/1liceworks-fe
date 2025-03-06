185d5256b35946f6cdbea361e9a76c0ed3469594
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-06 21:59:31 +0900

    design: 캘린더별 체크박스에 calendarColors 적용

--- src/features/calendar/ui/CalendarListItem.tsx
+++ src/features/calendar/ui/CalendarListItem.tsx
@@ -2,6 +2,8 @@ import { useCalendarStore } from '@/features/calendar/model/useCalendarStore';
 import { Checkbox } from '@/shared/ui/shadcn/Checkbox';
 import { Label } from '@/shared/ui/shadcn/Label';
 
+import { calendarColors } from '../model/constants';
+
 interface CalendarListItemProps {
   calendarId: number | undefined;
   calendarName: string | undefined;
@@ -20,21 +22,20 @@ export const CalendarListItem = ({
 
   const isChecked = checkedCalendarIds.includes(calendarId);
 
-  const handleCheckedChange = (checked: boolean) => {
+  const handleCheckedChange = () => {
     toggleCalendar(calendarId);
-    console.log(
-      `Calendar ${calendarName} (ID: ${calendarId}) is now ${checked ? 'checked' : 'unchecked'}`
-    );
   };
+
+  const color = calendarColors[calendarId];
 
   return (
     <div className='flex items-center gap-x-2'>
-      {/* TODO) calendarId에 따라 Checkbox border, background color 적용 */}
       <Checkbox
         id={calendarId?.toString()}
         className='rounded-full'
         checked={isChecked}
         onCheckedChange={handleCheckedChange}
+        style={{ backgroundColor: color, borderColor: color }}
       />
       <Label htmlFor={calendarId?.toString()} className='font-normal'>
         {calendarName}
