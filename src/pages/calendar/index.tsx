12fbb660947b97367f50a23f9e281faba3fd1dc3
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-14 20:07:00 +0900

    feat: Calendar API 연동 및 날짜 변경 시 이벤트 조회 구현
    - onNavigate 콜백으로 날짜 변경 감지 및 상태 관리 추가
    - 날짜 변경 시 자동으로 API 요청 수행하도록 개선
    - useCalendarEvents 커스텀 훅을 사용해 캘린더 이벤트 조회

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,9 +1,15 @@
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { Calendar, dayjsLocalizer } from 'react-big-calendar';
 
+import { CalendarListDTO } from '@/features/calendar/api/dto';
+import {
+  calendarQueries,
+  useCalendarEvents,
+} from '@/features/calendar/api/queries';
 import { mockCalendars } from '@/features/calendar/model/mockData';
 import { useCalendarStore } from '@/features/calendar/model/useCalendarStore';
 import { CustomEvents } from '@/features/calendar/ui/CustomEvents';
@@ -12,10 +18,28 @@ import { CustomToolbar } from '@/features/calendar/ui/CustomToolbar';
 const localizer = dayjsLocalizer(dayjs);
 
 export const CalendarPage = () => {
+  const [currDate, setCurrDate] = useState(new Date());
+
   const checkedCalendarIds = useCalendarStore(
     (state) => state.checkedCalendarIds
   );
 
+  const { data: calendars } = useQuery<CalendarListDTO>({
+    ...calendarQueries.getCalendars,
+  });
+
+  // useCalendarEvents 커스텀 훅 사용
+  const calendarEventsQueries = useCalendarEvents(
+    checkedCalendarIds,
+    calendars,
+    currDate
+  );
+
+  const eventsData = calendarEventsQueries.map((query) => query.data);
+  console.log('데이터', eventsData);
+
+  // TODO) eventsToDisplay를 eventsData로 대체
+  // TODO) filteredCalendars는 제거
   const filteredCalendars = useMemo(
     () =>
       mockCalendars.filter((calendar) =>
@@ -40,10 +64,12 @@ export const CalendarPage = () => {
         localizer={localizer}
         events={eventsToDisplay}
         onSelectEvent={(e) => console.log(e)}
+        onNavigate={(newDate) => setCurrDate(newDate)}
         components={{
           toolbar: (props) => <CustomToolbar {...props} />,
           event: (props) => <CustomEvents {...props} />,
         }}
+        showAllEvents
       />
     </div>
   );
