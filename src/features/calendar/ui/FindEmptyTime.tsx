b55f025116c90028d9c2c4c57af1cfef7e8331ea
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-19 21:52:57 +0900

    refactor: FindEmptyTime 컴포넌트 렌더링 로직 이동 및 역할 분리
    - FindEmptyTime 컴포넌트는 수정 모드일 때만 렌더링되도록 변경 (EventEditForm으로 이동)
    - FindEmptyTime 컴포넌트를 상위 컴포넌트인 EventDetails에서 캘린더 타입에 따라 조건부로 렌더링하도록 변경
    - FindEmptyTime은 이제 UI만 담당하고, calendarType에 대한 로직은 상위 컴포넌트에서 처리하도록 역할 분리

--- src/features/calendar/ui/FindEmptyTime.tsx
+++ src/features/calendar/ui/FindEmptyTime.tsx
@@ -1,23 +1,13 @@
 import { Button } from '@/shared/ui/shadcn/Button';
 
-interface FindEmptyTimeProps {
-  // calendarType: CalendarTypeDTO;
-  calendarType: number;
-}
-
-export const FindEmptyTime = ({ calendarType }: FindEmptyTimeProps) => {
+export const FindEmptyTime = () => {
   return (
-    <div className='bg-background-light flex h-full w-full max-w-80 flex-col items-center justify-center gap-4 rounded-lg border'>
-      {calendarType === 1 && (
-        <>
-          <p className='text-muted-foreground text-sm'>
-            회의 시간과 참석자를 선택하셨나요?
-            <br />
-            모두가 가능한 회의 시간을 찾아보세요.
-          </p>
-          <Button size='sm'>빈 시간 찾기</Button>
-        </>
-      )}
+    <div className='bg-background-light flex h-full w-full max-w-80 min-w-fit flex-col items-center justify-center gap-4 rounded-lg border p-4'>
+      <div className='text-muted-foreground text-center text-sm'>
+        <p>회의 시간과 참석자를 선택하셨나요?</p>
+        <p>모두가 가능한 회의 시간을 찾아보세요.</p>
+      </div>
+      <Button size='sm'>빈 시간 찾기</Button>
     </div>
   );
 };
