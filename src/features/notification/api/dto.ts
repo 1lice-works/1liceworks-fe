541b361e979a444b7376e2c4e9b3f45af031a3f0
[PT1] 양혜림 <hlhlstar@naver.com>
2025-03-29 22:28:08 +0900

    feat: 알림 조회 API 응답 전체  DTO 추가
    - 기존 NotificationDTO를 활용하여 응답 데이터 구조 확장

--- src/features/notification/api/dto.ts
+++ src/features/notification/api/dto.ts
@@ -1,3 +1,10 @@
+export interface NotificationResponseDTO {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  result: NotificationDTO[];
+}
+
 export interface NotificationDTO {
   notificationId: number;
   message: string;
